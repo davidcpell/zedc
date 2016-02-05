7206def771b7cf8b8cc886df81172cdcdcae22a0
David Pell <davidcpell@gmail.com>
2016-02-05 00:08:21 -0500

    First commit. Finish ex1.

---
+++ 1/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  puts("Hello world.");
+  puts("This sentence\thas a tab.");
+  puts("****************");
+  puts("Ok here's the fourth sentence.");
+  puts("I can do this all day!");
+
+  return 0;
+}
